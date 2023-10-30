ae2581de9387593e1a9cb24df54d1bc238f5f89a
marctche <146140837+marctche@users.noreply.github.com>
2023-10-29 22:54:06 -0400

    LearnMore.js

---
+++ LearnMore.js
@@ -0,0 +1,21 @@
+
+
+    <script>
+        function toggleAboutText() {
+            const aboutText = document.getElementById("about-text");
+            const learnMoreButton = document.getElementById("learn-more-button");
+            
+            if (aboutText.style.display === "none" || aboutText.style.display === "") {
+            
+                aboutText.style.display = "block";
+                learnMoreButton.innerHTML = "Show Less"; 
+            } 
+            
+            else {
+                aboutText.style.display = "none";
+                learnMoreButton.innerHTML = "Learn More"; 
+            }
+        }
+    </script>
+</body>
+</html>
